3ba4c6427d7698426cd4898e352d15a3c40ab3ce
anushalihala <anusha.lihala@gmail.com>
2018-10-19 20:10:35 +0530

    refactor activation.py

--- from-scratch/activation.py
+++ from-scratch/activation.py
@@ -1,19 +1,23 @@
 import numpy as np
 
-def sigmoid(x):
-    return 1/(1+np.exp(np.multiply(x,-1)))
+class Sigmoid:
+    def apply(x):
+        return 1/(1+np.exp(np.multiply(x,-1)))
 
-def sigmoid_grad(x):
-    return sigmoid(x)*(1-sigmoid(x))
+    def grad(x):
+        return sigmoid(x)*(1-sigmoid(x))
 
-def relu(x):
-    return np.maximum(x,0)
+class Relu:
+    def apply(x):
+        return np.maximum(x,0)
 
-def relu_grad(x):
-    return np.where(np.array(x)>=0, 1, 0)
-    
-def tanh(x):
-    return np.tanh(x)
+    def grad(x):
+        return np.where(np.array(x)>=0, 1, 0)
 
-def tanh_grad(x):
-    return 1-tanh(x)**2
+class Tanh:
+    def apply(x):
+        return np.tanh(x)
+
+    def grad(x):
+        return 1-tanh(x)**2
+        
