45b63de3da840f8ee054be3ace8f460cba853e9c
anushalihala <anusha.lihala@gmail.com>
2018-10-22 01:49:48 +0530

    debug loss.py
    correct cost function

--- from-scratch/loss.py
+++ from-scratch/loss.py
@@ -1,14 +1,13 @@
 import numpy as np
     
 class LogisticLoss:
-    def apply(x):
+    def apply_activation(x):
         return 1/(1+np.exp(np.multiply(x,-1)))
         
-    def cost(Y, Z_L):
-        A_L = LogisticLoss.apply(Z_L) 
+    def get_loss(Y, A_L):
         m = Y.shape[1]
 
-        cost = (-1/m)*(np.dot(Y,np.log(A_L).T) + ((1-Y) * np.log(1-A_L)) )
+        cost = (-1/m)*np.sum(np.dot(Y,np.log(A_L).T)+np.dot((1-Y),np.log(1-A_L).T))
 
         cost = np.squeeze(cost)   
         assert(cost.shape == ())
