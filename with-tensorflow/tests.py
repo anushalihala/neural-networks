05ea51da7ea8da78252b053fbaffb1360fdb3767
anushalihala <anusha.lihala@gmail.com>
2018-10-24 22:12:57 +0530

    update tests.py
    modify parameters of neural network for xor gate

--- with-tensorflow/tests.py
+++ with-tensorflow/tests.py
@@ -34,18 +34,30 @@ X = np.array([[0,0,1,1], \
 Y = np.array([[0,1,1,0]])
 loss = lambda y,z: tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=tf.transpose(z),labels=tf.transpose(y)))
 nn = NeuralNetwork([2,1])
-nn.train(X, Y, loss, epochs=500, learning_rate=1, output_frequency=100)
+nn.train(X, Y, loss, epochs=2000, learning_rate=0.02, output_frequency=500)
 print(nn.predict(X))
 print()
 del nn
 
-#xor gate success
+#xor gate success  - slow
 X = np.array([[0,0,1,1], \
               [0,1,0,1]])
 Y = np.array([[0,1,1,0]])
 loss = lambda y,z: tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=tf.transpose(z),labels=tf.transpose(y)))
 nn = NeuralNetwork([2,2,1])
-nn.train(X, Y, loss, epochs=1000, learning_rate=1, output_frequency=100)
+nn.train(X, Y, loss, epochs=20000, learning_rate=0.02, output_frequency=500)
+print(nn.predict(X))
+nn.write_graph()
+print()
+del nn
+
+#xor gate success  - fast
+X = np.array([[0,0,1,1], \
+              [0,1,0,1]])
+Y = np.array([[0,1,1,0]])
+loss = lambda y,z: tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=tf.transpose(z),labels=tf.transpose(y)))
+nn = NeuralNetwork([2,4,2,1])
+nn.train(X, Y, loss, epochs=10000, learning_rate=0.025, output_frequency=500)
 print(nn.predict(X))
 nn.write_graph()
 print()
